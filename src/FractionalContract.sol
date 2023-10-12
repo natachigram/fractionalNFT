60c7e45121d3d4642e01ff25f57d48110b5cf474
MACBOOK <natachigram@gmail.com>
2023-10-12 19:05:35 +0100

    fractional init

---
+++ src/FractionalContract.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Fraktal1155 is ERC1155, Ownable {
+    uint256 public _tokenIds;
+
+    uint256 public platformFee = 1; // 0.1%
+
+    // Mapping to store the token shares owned by each address.
+    mapping(uint256 => mapping(address => uint256)) public sharesBalances;
+
+    event SharesPurchased(
+        address indexed buyer,
+        uint256 tokenId,
+        uint256 amount,
+        uint256 cost
+    );
+    event SharesTransferred(
+        address indexed from,
+        address indexed to,
+        uint256 tokenId,
+        uint256 amount
+    );
+    event Minted(uint256 tokenId, string uri);
+
+    constructor(string memory uri) ERC1155(uri) {
+        // Initialize your contract with a base URI
+    }
+
+    constructor() public {
+        mintingFee = 0;
+    }
+
+    function setURI(string memory newURI) external onlyOwner {
+        _setURI(newURI);
+    }
+
+    function mintNFT(string memory tokenURI) external onlyOwner {
+        _tokenIds++;
+        uint256 tokenId = _tokenIds;
+        _mint(msg.sender, tokenId, 1, "");
+        sharesBalances[tokenId][msg.sender] = 10000; // 100% shares to the owner
+        emit Minted(tokenId, tokenURI);
+    }
+
+    function purchaseShares(uint256 tokenId, uint256 amount) external payable {
+        uint256 cost = calculateCost(tokenId, amount);
+        require(msg.value >= cost, "Insufficient payment");
+
+        uint256 platformFeeAmount = (cost * platformFee) / 10000;
+        uint256 ownerAmount = cost - platformFeeAmount;
+
+        address owner = ownerOf(tokenId);
+
+        sharesBalances[tokenId][owner] -= amount;
+        sharesBalances[tokenId][msg.sender] += amount;
+
+        owner.transfer(ownerAmount);
+        payable(owner()).transfer(platformFeeAmount);
+
+        emit SharesPurchased(msg.sender, tokenId, amount, cost);
+    }
+
+    function transferShares(
+        uint256 tokenId,
+        address to,
+        uint256 amount
+    ) external {
+        require(
+            sharesBalances[tokenId][msg.sender] >= amount,
+            "Insufficient shares"
+        );
+        sharesBalances[tokenId][msg.sender] -= amount;
+        sharesBalances[tokenId][to] += amount;
+        emit SharesTransferred(msg.sender, to, tokenId, amount);
+    }
+
+    function calculateCost(
+        uint256 tokenId,
+        uint256 amount
+    ) public view returns (uint256) {
+        uint256 sharesPrice = calculateSharesPrice(tokenId);
+        return sharesPrice * amount;
+    }
+
+    function calculateSharesPrice(
+        uint256 tokenId
+    ) public view returns (uint256) {
+        return
+            (totalSupply(tokenId) * 1 ether) /
+            sharesBalances[tokenId][address(this)];
+    }
+}
---
+++ src/NFTMarketplace.sol
@@ -0,0 +1,127 @@
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./FractionalContract.sol";
+
+contract NFTMarketplace is Ownable {
+    struct Listing {
+        uint256 tokenId;
+        uint256 price;
+        uint256 numberOfShares;
+    }
+
+    FractionalContract public fractionalContract; // Reference to the Fraktal1155 contract
+
+    // STORAGE
+    mapping(uint256 => Listing) public listings;
+    uint256 public fee;
+    // EVENTS
+    event FeeUpdated(uint256 newFee);
+    event ItemListed(uint256 tokenId, uint256 price, uint256 amountOfShares);
+    event ItemPriceUpdated(uint256 tokenId, uint256 newPrice);
+    event ItemPurchased(
+        uint256 tokenId,
+        address indexed buyer,
+        uint256 sharesPurchased,
+        uint256 totalCost
+    );
+    event ItemUnlisted(uint256 tokenId);
+
+    constructor(address _fractionalContractAddress) {
+        fractionalContract = FractionalContract(_fractionalContractAddress);
+        fee = 0; // Initialize the fee to 0
+    }
+
+    function setFee(uint256 _newFee) external onlyOwner {
+        require(_newFee >= 0, "NFTMarketplace: Negative fee not acceptable");
+        fee = _newFee;
+        emit FeeUpdated(_newFee);
+    }
+
+    // List an NFT with its ID, price, and number of shares.
+    function listNFT(
+        uint256 _tokenId,
+        uint256 _price,
+        uint256 _numberOfShares
+    ) external onlyOwner {
+        require(_price > 0, "NFTMarketplace: Price must be greater than zero");
+        require(
+            _numberOfShares > 0,
+            "NFTMarketplace: Number of shares must be greater than zero"
+        );
+        require(
+            fractionalContract.ownerOf(_tokenId) == address(this),
+            "NFTMarketplace: Contract does not own the NFT"
+        );
+
+        Listing storage listing = listings[_tokenId];
+        listing.tokenId = _tokenId;
+        listing.price = _price;
+        listing.numberOfShares = _numberOfShares;
+
+        emit ItemListed(_tokenId, _price, _numberOfShares);
+    }
+
+    // Update the price of an existing listing.
+    function updatePrice(
+        uint256 _tokenId,
+        uint256 _newPrice
+    ) external onlyOwner {
+        require(
+            _newPrice > 0,
+            "NFTMarketplace: Price must be greater than zero"
+        );
+        Listing storage listing = listings[_tokenId];
+        require(listing.tokenId != 0, "NFTMarketplace: Invalid token id");
+        listing.price = _newPrice;
+        emit ItemPriceUpdated(_tokenId, _newPrice);
+    }
+
+    // Purchase shares of an NFT from the marketplace.
+    function purchaseShares(
+        uint256 _tokenId,
+        uint256 _sharesToPurchase
+    ) external payable nonReentrant {
+        Listing storage listing = listings[_tokenId];
+        require(listing.tokenId != 0, "NFTMarketplace: Invalid token id");
+        require(
+            _sharesToPurchase > 0,
+            "NFTMarketplace: Number of shares to purchase must be greater than zero"
+        );
+        require(
+            listing.numberOfShares >= _sharesToPurchase,
+            "NFTMarketplace: Requested shares amount exceeds balance"
+        );
+        uint256 totalPrice = listing.price * _sharesToPurchase;
+        require(msg.value >= totalPrice, "NFTMarketplace: Insufficient funds");
+
+        // Calculate fees
+        uint256 platformFeeAmount = (totalPrice * fee) / 10000; // fee is in percentage
+        uint256 sellerAmount = totalPrice - platformFeeAmount;
+
+        // Transfer shares to the buyer
+        fractionalContract.safeTransferFrom(
+            address(this),
+            msg.sender,
+            _tokenId,
+            _sharesToPurchase,
+            ""
+        );
+
+        // Transfer funds to the seller and platform
+        payable(owner()).transfer(platformFeeAmount);
+        payable(fractionalContract.ownerOf(_tokenId)).transfer(sellerAmount);
+
+        // Update the number of shares available in the listing
+        listing.numberOfShares = listing.numberOfShares - _sharesToPurchase;
+
+        emit ItemPurchased(_tokenId, msg.sender, _sharesToPurchase, totalPrice);
+    }
+
+    // Unlist an NFT from the marketplace.
+    function unlistNFT(uint256 _tokenId) external onlyOwner {
+        Listing storage listing = listings[_tokenId];
+        require(listing.tokenId != 0, "NFTMarketplace: Invalid token id");
+        delete listings[_tokenId];
+        emit ItemUnlisted(_tokenId);
+    }
+}
